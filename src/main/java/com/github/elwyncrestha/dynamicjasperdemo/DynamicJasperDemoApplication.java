e05d69b193e39aca4394eabb0cb6a8af521c4dca
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-04 17:54:44 +0545

    Initial Commit

---
+++ src/main/java/com/github/elwyncrestha/dynamicjasperdemo/DynamicJasperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.elwyncrestha.dynamicjasperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicJasperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicJasperDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/elwyncrestha/dynamicjasperdemo/DynamicJasperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elwyncrestha.dynamicjasperdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicJasperDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
